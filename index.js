c156532daa17deea897db7f2a7fe6e547173286c
msmsoft <msm2007@gmail.com>
2019-09-01 00:56:01 -0500

    Weather app done

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const http = require('http');
-const fs = require('fs');
-// Server configuration
-const hostname = '127.0.0.1';
-const port = 8080;
-// Render page
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    const server = http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(port, hostname, () => {
-        console.log(`Server running at http://${hostname}:${port}/`);
-    });
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+//add the router
+app.use(express.static(__dirname + '/'));
+//Store all HTML files in view folder.
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/js'));
+//Store all JS and CSS in Scripts folder.
+
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
