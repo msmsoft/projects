acffce02727c47f8ddb6247231a1e3d38fb9b70e
msmsoft <msm2007@gmail.com>
2020-08-28 12:46:51 -0500

    New controller created

---
+++ js/controllers/componentsCtrl.js
@@ -0,0 +1,10 @@
+(function () {
+  'use strict';
+  angular
+    .module('app')
+    .controller('componentsCtrl',  componentsCtrl);
+  function componentsCtrl($scope, dataService) {
+    var vm = this;
+
+  };
+})();
